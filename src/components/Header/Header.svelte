c22a3e255499c4dd8e8cbddb14833180486af67e
Jesse Kroon <156132898+jesse-kroon@users.noreply.github.com>
2024-11-12 16:18:58 +0100

    get correct pathing for static image

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,7 +1,11 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <header class="p-4 text-white">
 	<nav class="flex justify-between">
 		<div class="flex gap-2">
-			<img class="rounded-full" src="/images/profile.jpeg" alt="" width="28" height="28" />
+			<img class="rounded-full" src="{base}/images/profile.jpeg" alt="" width="28" height="28" />
 			<h1>Jesse Kroon</h1>
 		</div>
 		<div class="flex justify-between gap-2">
