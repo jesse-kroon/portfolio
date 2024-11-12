aae9d553a4a946b0e2113515424629969aad06d4
Jesse Kroon <156132898+jesse-kroon@users.noreply.github.com>
2024-11-12 16:17:30 +0100

    get rid of fixed footer

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="fixed bottom-0 flex w-full flex-col bg-indigo-950 p-2 text-sm text-white">
+<footer class="bottom-0 flex w-full flex-col bg-indigo-950 p-2 text-sm text-white">
 	<section class="flex flex-col">
 		<h2 class="text-lg font-bold text-amber-400">Online coding presence</h2>
 		<a href="https://www.frontendmentor.io/profile/jesse-kroon">Frontend Mentor</a>
