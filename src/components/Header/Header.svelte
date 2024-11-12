b6638b14d3740c1ebc5c21d4d05d21502e5840eb
Jesse Kroon <156132898+jesse-kroon@users.noreply.github.com>
2024-11-12 15:14:35 +0100

    add some more layout

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,12 @@
+<header class="bg-stone-800 p-2">
+	<nav class="flex justify-between">
+		<div>
+			<img src="" alt="" />
+			<h1>Jesse Kroon</h1>
+		</div>
+		<div>
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+		</div>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import Footer from '../components/Footer/Footer.svelte';
+	import Header from '../components/Header/Header.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Header />
+<main class="container mx-auto">
+	{@render children()}
+</main>
+<Footer />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.slate.950);
+	}
+</style>
